2b227de35d4dbb5589caf583edb3b3f6b7073f79
Sriram <46366477+ram170@users.noreply.github.com>
2020-05-17 01:59:18 +0530

    Updates

---
+++ precache-manifest.65d5208049ef8355ae06c79c2e19c6d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b415b8b603fd9a1f521fddea819ef9a0",
+    "url": "/MovieWiki/index.html"
+  },
+  {
+    "revision": "4702cd3811d833c261db",
+    "url": "/MovieWiki/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bdcb2f09c5bd9af913b5",
+    "url": "/MovieWiki/static/css/main.776fb2a9.chunk.css"
+  },
+  {
+    "revision": "4702cd3811d833c261db",
+    "url": "/MovieWiki/static/js/2.6ea1de7c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MovieWiki/static/js/2.6ea1de7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdcb2f09c5bd9af913b5",
+    "url": "/MovieWiki/static/js/main.d260e541.chunk.js"
+  },
+  {
+    "revision": "8605a01badf524716a39",
+    "url": "/MovieWiki/static/js/runtime-main.39508865.js"
+  }
+]);
